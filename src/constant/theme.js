741f227991a11fd06bbee08995ff37d6740b1010
dabin <dabin219@naver.com>
2022-01-20 18:28:18 +0900

    ADD: dark mode & todo 추가

---
+++ src/constant/theme.js
@@ -0,0 +1,12 @@
+export const THEMES = {
+  light: {
+    backgroudColor: "#EEEEEE",
+    color: "#333333",
+    buttonColor: "orange",
+  },
+  dark: {
+    backgroundColor: "#333333",
+    color: "#EEEEEE",
+    buttonColor: "yellow",
+  },
+};
